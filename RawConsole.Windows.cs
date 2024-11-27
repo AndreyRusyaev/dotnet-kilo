0b8ce69c2ca6d993f46c8bc28cb96c6067b698a9
Andrey Rusyaev <Andrey.Rusyaev@gmail.com>
2024-11-27 15:52:27 +0400

    Switch to use cfmakeraw for Unix to avoid hardcoding raw mode flags (platform dependent). Small refactor interops.

--- RawConsole.Windows.cs
+++ RawConsole.Windows.cs
@@ -3,51 +3,40 @@ using System.Runtime.InteropServices;
 
 internal static partial class RawConsole
 {
-    private static int STD_INPUT_HANDLE = -10;
-
-    private static int STD_OUTPUT_HANDLE = -11;
-
-    internal const short KEY_EVENT = 1;
-
     private static char[] windowsKeyBuf = new char[1];
 
     private static void EnableRawModeWindows()
     {
-        var stdIn = GetStdHandle(STD_INPUT_HANDLE);
-        var stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        var stdIn = Kernel32.GetStdHandle(Kernel32.STD_INPUT_HANDLE);
+        var stdOut = Kernel32.GetStdHandle(Kernel32.STD_OUTPUT_HANDLE);
 
-        if (!GetConsoleMode(stdIn, out int nativeInputMode))
+        if (!Kernel32.GetConsoleMode(stdIn, out int nativeInputMode))
         {
             throw new Win32Exception(Marshal.GetLastWin32Error());
         }
 
-        var originalInputMode = (ConsoleInputModes)nativeInputMode;
-
-        var newInputMode = originalInputMode;
-        newInputMode &= ~ConsoleInputModes.ENABLE_ECHO_INPUT;
-        newInputMode &= ~ConsoleInputModes.ENABLE_LINE_INPUT;
-        newInputMode &= ~ConsoleInputModes.ENABLE_PROCESSED_INPUT;
-        newInputMode |= ConsoleInputModes.ENABLE_VIRTUAL_TERMINAL_INPUT;
-
-        if (!GetConsoleMode(stdOut, out int nativeOutputMode))
+        if (!Kernel32.GetConsoleMode(stdOut, out int nativeOutputMode))
         {
             throw new Win32Exception(Marshal.GetLastWin32Error());
         }
-        var originalOutputMode = (ConsoleOutputModes)nativeOutputMode;
         
-        var newOutputMode = originalOutputMode;
-        newOutputMode &= ~ConsoleOutputModes.ENABLE_WRAP_AT_EOL_OUTPUT;
-        newOutputMode |= ConsoleOutputModes.ENABLE_PROCESSED_OUTPUT;
-        newOutputMode |= ConsoleOutputModes.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        var originalInputMode = (Kernel32.ConsoleInputModes)nativeInputMode;
+        var originalOutputMode = (Kernel32.ConsoleOutputModes)nativeOutputMode;        
 
-        if (!SetConsoleMode(stdIn, (int)newInputMode))
+        var newInputMode = originalInputMode;        
+        var newOutputMode = originalOutputMode;
+
+        MakeRawInputMode(ref newInputMode);
+        MakeRawOutputMode(ref newOutputMode);
+
+        if (!Kernel32.SetConsoleMode(stdIn, (int)newInputMode))
         {
-            throw new Win32Exception(Marshal.GetLastWin32Error());
+            throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to set stdin console mode.");
         }
 
-        if (!SetConsoleMode(stdOut, (int)newOutputMode))
+        if (!Kernel32.SetConsoleMode(stdOut, (int)newOutputMode))
         {
-            throw new Win32Exception(Marshal.GetLastWin32Error());
+            throw new Win32Exception(Marshal.GetLastWin32Error(), "Failed to set stdout console mode.");
         }
     }
 
@@ -55,7 +44,7 @@ internal static partial class RawConsole
     {
         while(true)
         {
-            if (!ReadConsoleW(GetStdHandle(STD_INPUT_HANDLE), windowsKeyBuf, 1, out int readBytes, IntPtr.Zero))
+            if (!Kernel32.ReadConsoleW(Kernel32.GetStdHandle(Kernel32.STD_INPUT_HANDLE), windowsKeyBuf, 1, out int readBytes, IntPtr.Zero))
             {
                 throw new Win32Exception(Marshal.GetLastWin32Error());
             }
@@ -68,41 +57,64 @@ internal static partial class RawConsole
             return windowsKeyBuf[0];
         }
     }
-    
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern IntPtr GetStdHandle(int nStdHandle);
 
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out int lpMode);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool SetConsoleMode(IntPtr hConsoleHandle, int dwMode);
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-    private static extern bool ReadConsoleW(IntPtr hConsoleHandle, char[] buffer, int nNumberOfCharsToRead, out int lpNumberOfCharsRead, IntPtr pInputControl);
-
-    [Flags]
-    private enum ConsoleInputModes : uint
+    private static void MakeRawInputMode(ref Kernel32.ConsoleInputModes inputMode)
     {
-        ENABLE_PROCESSED_INPUT = 0x0001,
-        ENABLE_LINE_INPUT = 0x0002,
-        ENABLE_ECHO_INPUT = 0x0004,
-        ENABLE_WINDOW_INPUT = 0x0008,
-        ENABLE_MOUSE_INPUT = 0x0010,
-        ENABLE_INSERT_MODE = 0x0020,
-        ENABLE_QUICK_EDIT_MODE = 0x0040,
-        ENABLE_EXTENDED_FLAGS = 0x0080,
-        ENABLE_AUTO_POSITION = 0x0100,
-        ENABLE_VIRTUAL_TERMINAL_INPUT = 0x200
+        inputMode &= ~Kernel32.ConsoleInputModes.ENABLE_ECHO_INPUT;
+        inputMode &= ~Kernel32.ConsoleInputModes.ENABLE_LINE_INPUT;
+        inputMode &= ~Kernel32.ConsoleInputModes.ENABLE_PROCESSED_INPUT;
+        inputMode |= Kernel32.ConsoleInputModes.ENABLE_VIRTUAL_TERMINAL_INPUT;
     }
 
-    [Flags]
-    private enum ConsoleOutputModes : uint
+    private static void MakeRawOutputMode(ref Kernel32.ConsoleOutputModes outputMode)
+    {
+        outputMode &= ~Kernel32.ConsoleOutputModes.ENABLE_WRAP_AT_EOL_OUTPUT;
+        outputMode |= Kernel32.ConsoleOutputModes.ENABLE_PROCESSED_OUTPUT;
+        outputMode |= Kernel32.ConsoleOutputModes.ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    }
+    
+    internal static class Kernel32
+    {
+        public const int STD_INPUT_HANDLE = -10;
+
+        public const int STD_OUTPUT_HANDLE = -11;
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetConsoleMode(IntPtr hConsoleHandle, out int lpMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int dwMode);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool ReadConsoleW(IntPtr hConsoleHandle, char[] buffer, int nNumberOfCharsToRead, out int lpNumberOfCharsRead, IntPtr pInputControl);
+
+        [Flags]
+        public enum ConsoleInputModes : uint
+        {
+            ENABLE_PROCESSED_INPUT = 0x0001,
+            ENABLE_LINE_INPUT = 0x0002,
+            ENABLE_ECHO_INPUT = 0x0004,
+            ENABLE_WINDOW_INPUT = 0x0008,
+            ENABLE_MOUSE_INPUT = 0x0010,
+            ENABLE_INSERT_MODE = 0x0020,
+            ENABLE_QUICK_EDIT_MODE = 0x0040,
+            ENABLE_EXTENDED_FLAGS = 0x0080,
+            ENABLE_AUTO_POSITION = 0x0100,
+            ENABLE_VIRTUAL_TERMINAL_INPUT = 0x200
+        }
+
+        [Flags]
+        public enum ConsoleOutputModes : uint
     {
         ENABLE_PROCESSED_OUTPUT = 0x0001,
         ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002,
         ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004,
         DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
         ENABLE_LVB_GRID_WORLDWIDE = 0x0010
+    }
     }
 }
